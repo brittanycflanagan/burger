3e8b2fa8277837567c9b5d8c9cfd463d65c3f7e2
brittanycflanagan <brittanycturner@gmail.com>
2018-05-06 08:51:49 -0700

    getting started

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger, devoured) VALUES ('Bacon cheeseburger', false);
+INSERT INTO burgers (burger, devoured) VALUES ('Green chile burger', false);
+INSERT INTO burgers (burger, devoured) VALUES ('Veggie burger', false);
